6e568f0daad21e46c108adc4f3aaf6d25e473fe6
melfuechec <melular@gmail.com>
2019-06-15 18:06:04 -0500

    changed image src

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout'
 import Head from '../components/Head'
+import { booboo } from './boo-boo.png'
 
 const AboutPage = () => {
     return (
@@ -11,7 +12,7 @@ const AboutPage = () => {
             <p>Hi I'm Kristie!</p>
             <p>I word the best words!</p>
             <p><Link to='/contact'>Want to work with me? Reach out! </Link></p>
-            <img alt='boo-boo' width="200" height="200" src='./boo-boo.png'></img>
+            <img alt='boo-boo' width="200" height="200" src={booboo}></img>
         </Layout>
     )
 }
