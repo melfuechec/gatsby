728f40084c4f5475c505ec7df917dcd665585991
melfuechec <melular@gmail.com>
2019-06-15 18:33:51 -0500

    img src

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout'
 import Head from '../components/Head'
-import { booboo } from "./boo-boo.png"
+//import { booboo } from "./boo-boo.png"
 
 const AboutPage = () => {
     return (
@@ -12,7 +12,7 @@ const AboutPage = () => {
             <p>Hi I'm Kristie!</p>
             <p>I word the best words!</p>
             <p><Link to='/contact'>Want to work with me? Reach out! </Link></p>
-            <img alt='boo-boo' width="200" height="200" src={booboo}></img>
+            <img alt='boo-boo' width="200" height="200" src={'https://scontent.faus1-1.fna.fbcdn.net/v/t1.0-9/51484159_10201462873710855_4412324322425700352_n.jpg?_nc_cat=109&_nc_oc=AQnQPuYqbrd4z9KULS3MZw8rCKHnszjWSecUmC-63RoXyFtrwdekD12LR4BYzpJlx3qKhoB7KGE4FRQPl6s6SE_O&_nc_ht=scontent.faus1-1.fna&oh=28e2be79ae6bf4841b479c027e9d911a&oe=5D7E0BD0'}></img>
         </Layout>
     )
 }
